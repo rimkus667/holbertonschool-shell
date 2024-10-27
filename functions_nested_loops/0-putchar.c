8344edee06bff0c0aae38a1a746d2bae3a37d13d
root <karimhabouchi310@gmail.com>
2024-10-27 12:30:45 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+/*betty style doc for function main goes there */
+/**
+* main -  prints _putchar, followed by a new line
+*
+* Return: Always 0 Success
+*/
+int main(void)
+{
+putchar('_putchar');
+putchar(/n);
+return 0
+}
